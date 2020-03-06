6e35607737e94e72ff73f90fe7eec13d6e9d660d
Sophia <gmb728@gmail.com>
2020-03-06 13:13:50 +0800

    Initial Commit

---
+++ SwiftUIStackPrice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStackPrice
+//
+//  Created by Chang Sophia on 3/6/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
